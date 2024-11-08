0dc368399fddf060d45f1fce3f9e20b56eff5964
DimasOrtiz2703 <dimasortiz915@gmail.com>
2024-11-08 15:20:26 -0600

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,6 @@
+var nombre="Dimas";
+function contarletras(nombre){
+    return nombre.length;
+}
+var numerodeLetras = contarletras (nombre)
+console.log("el numero de letras en el nombre es:"+ numerodeLetras);
